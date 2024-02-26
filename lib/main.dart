3c69dec483b5ce59e1af00ad693adabf4fe39cde
Gokulnath Gopu <gokulnathgopu@Gokulnaths-MacBook-Air.local>
2024-02-27 01:28:36 +0530

    ScreenUtilInit pachage added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_config/flutter_config.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nested/nested.dart';
 import 'app_config.dart';
 import 'base_bloc/base_bloc.dart';
@@ -28,13 +29,10 @@ Future<void> main() async {
 
   Bloc.observer = AppBlocObserver();
 
-  runApp(
-      MultiBlocProvider(
-          providers: <SingleChildWidget>[
-            BlocProvider<AppBloc>(create: (BuildContext context) => appBloc),
-            BlocProvider<AuthBloc>(create: (BuildContext context) => authBloc),
-          ],
-          child: const MyApp()));
+  runApp(MultiBlocProvider(providers: <SingleChildWidget>[
+    BlocProvider<AppBloc>(create: (BuildContext context) => appBloc),
+    BlocProvider<AuthBloc>(create: (BuildContext context) => authBloc),
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -45,7 +43,6 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> with WidgetsBindingObserver {
-
   @override
   void initState() {
     super.initState();
@@ -55,15 +52,18 @@ class MyAppState extends State<MyApp> with WidgetsBindingObserver {
 
   Future<void> _init() async {}
 
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      navigatorKey: navigatorKey,
-      theme: themeData,
-      title: 'MyApp',
-      home: const InitPage(),
-      debugShowCheckedModeBanner: AppConfig.shared.flavor == Flavor.staging,
-    );
+    return ScreenUtilInit(
+        designSize: const Size(390, 835),
+        builder: (_, Widget? child) {
+          return MaterialApp(
+            navigatorKey: navigatorKey,
+            theme: themeData,
+            home: const InitPage(),
+            debugShowCheckedModeBanner:
+                AppConfig.shared.flavor == Flavor.staging,
+          );
+        });
   }
 }
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 final ThemeData themeData = ThemeData(
   colorScheme: ColorScheme.fromSeed(
       seedColor: Colors.lightBlueAccent,
+      error: Colors.redAccent
   ),
-  textTheme: const TextTheme(
-    titleLarge: TextStyle(fontWeight: FontWeight.w700),
-    titleMedium: TextStyle(fontWeight: FontWeight.w700),
-    titleSmall: TextStyle(fontWeight: FontWeight.w700),
-    bodyLarge: TextStyle(fontWeight: FontWeight.w500),
-    bodyMedium: TextStyle(fontWeight: FontWeight.w500),
-    bodySmall: TextStyle(fontWeight: FontWeight.w500),
+  textTheme: TextTheme(
+    titleLarge: TextStyle(fontWeight: FontWeight.w700, fontSize: 20.sp),
+    titleMedium: TextStyle(fontWeight: FontWeight.w700, fontSize: 18.sp),
+    titleSmall: TextStyle(fontWeight: FontWeight.w700, fontSize: 16.sp),
+    bodyLarge: TextStyle(fontWeight: FontWeight.w500, fontSize: 18.sp),
+    bodyMedium: TextStyle(fontWeight: FontWeight.w500, fontSize: 16.sp),
+    bodySmall: TextStyle(fontWeight: FontWeight.w500, fontSize: 14.sp),
   ),
 );
