72ec47a4b1df374c0904adc812952f9d5b54cd26
syed rahimuddin <syed.rahimuddin@rootquotient.com>
2025-02-11 11:48:13 +0530

    Updated boiler plate code to 3.27.1

--- lib/api_services/auth_service.dart
+++ lib/api_services/auth_service.dart
@@ -5,24 +5,24 @@ import '../models/app_user.dart';
 import '../models/token.dart';
 
 class AuthService extends ApiRepository {
-
 //************************************ log-in *********************************//
   Future<Map<String, dynamic>?> loginWithPassword(
       {Map<String, dynamic>? objToApi}) async {
-    final Response<dynamic> res = await ApiRepository.apiClient.post(
-      '/user_management/employee/login',
-      data: objToApi
-    );
-    return <String, dynamic>{'customer': AppUser.fromJson(res.data['employee'] as Map<String, dynamic>), 'token': Token.fromJson(res.data['token'] as Map<String, dynamic>)};
+    final Response<dynamic> res = await ApiRepository.apiClient
+        .post('/user_management/employee/login', data: objToApi);
+    return <String, dynamic>{
+      'customer': AppUser.fromJson((res.data
+          as Map<String, dynamic>)['employee'] as Map<String, dynamic>),
+      'token': Token.fromJson(
+          (res.data as Map<String, dynamic>)['token'] as Map<String, dynamic>)
+    };
   }
 
 //************************************ log-out *********************************//
-  Future<Response<dynamic>> logOut(
-      {Map<String, String>? headersToApi}) async {
+  Future<Response<dynamic>> logOut({Map<String, String>? headersToApi}) async {
     final Response<dynamic> res = await ApiRepository.apiClient.delete(
-      '/user_management/employee/logout',
-      options: Options(headers: headersToApi)
-    );
+        '/user_management/employee/logout',
+        options: Options(headers: headersToApi));
     return res;
   }
 }
